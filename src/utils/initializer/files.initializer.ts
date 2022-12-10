80be59250c6909a4fc648fe49114ce148af18a9a
KRSBX <firdaus.sati51@gmail.com>
2022-12-11 00:19:57 +0700

    fix: wrong .env name

--- src/utils/initializer/files.initializer.ts
+++ src/utils/initializer/files.initializer.ts
@@ -7,17 +7,15 @@ import { PKG_ROOT } from 'utils/constants';
 export const initializeFiles = async (projectDir: string, flags: CRE.CliFlags) => {
   const templateDir = path.join(PKG_ROOT, 'template');
 
-  // Just rename the constant names to make it more readable
-  const destDir = projectDir;
-
   const spinner = ora(`Initializing CRE...`).start();
 
   try {
-    await fs.copy(templateDir, destDir);
-    await fs.copy(path.join(destDir, '_env'), path.join(projectDir, '.env'));
-    await fs.copy(path.join(destDir, '_env'), path.join(projectDir, '.env.example'));
+    await fs.copy(templateDir, projectDir);
+    await fs.copy(path.join(projectDir, '_env'), path.join(projectDir, '.env.example'));
+    await fs.rename(path.join(projectDir, '_env'), path.join(projectDir, '.env'));
 
-    if (!flags.noGit) await fs.rm(path.join(destDir, '_gitignore'));
+    if (!flags.noGit) await fs.rm(path.join(projectDir, '_gitignore'));
+    else await fs.rename(path.join(projectDir, '_gitignore'), path.join(projectDir, '.gitignore'));
 
     spinner.succeed(chalk.green.bold('CRE initialized.'));
   } catch {
